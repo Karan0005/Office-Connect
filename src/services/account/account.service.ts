86cfc4b17fbe47e265ece4910622cc89c13918dd
Karan Gupta <karangupta0005@gmail.com>
2018-06-05 10:11:36 +0530

    Commit Account Service Architect

--- src/services/account/account.service.ts
+++ src/services/account/account.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Injectable()
 export class AccountService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  public signUp(params) {
+    const route: String = '/account/signUp';
+  }
+
+  public signIn(params) {
+    const route: String = '/account/signIn';
+  }
+
+  public signOut(params) {
+    const route: String = '/account/signOut';
+  }
+
+  public passwordReset(params) {
+    const route: String = '/account/passwordReset';
+  }
+
+  public accountActivate(params) {
+    const route: String = '/account/accountActivate';
+  }
+
+  public accountDeactivate(params) {
+    const route: String = '/account/accountDeactivate';
+  }
+
+  public accountDelete(params) {
+    const route: String = '/account/accountDelete';
+  }
 
 }
