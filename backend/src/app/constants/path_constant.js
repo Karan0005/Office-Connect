5f7c392bf93522d0ffc62f4f3172dc331d50b1d7
Karan Gupta <karangupta0005@gmail.com>
2018-06-20 10:08:18 +0530

    Commit Fix

--- backend/src/app/constants/path_constant.js
+++ backend/src/app/constants/path_constant.js
@@ -2,11 +2,11 @@
 
 module.exports = {
     "Config": "environment/config",
-    "AppControllers" : {
-        "CommonController" : "app/controllers/CommonController"
+    "AppControllers": {
+        "CommonController": "app/controllers/CommonController"
     },
-    "WebPages" : {
-        "Welcome" : "app/webpages/welcome.html",
-        "Error" : "app/webpages/error.html"
+    "WebPages": {
+        "Welcome": "app/webpages/welcome.html",
+        "Error": "app/webpages/error.html"
     }
 };
