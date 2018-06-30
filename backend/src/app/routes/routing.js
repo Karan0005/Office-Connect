75b595df10f2c539cac0b80c402fac4937906972
Karan Gupta <karangupta0005@gmail.com>
2018-06-30 20:47:42 +0530

    Commit Fix

--- backend/src/app/routes/routing.js
+++ backend/src/app/routes/routing.js
@@ -8,7 +8,9 @@ const reqModels = global._pathConstants.ReqModels;
 const accountModel = require(reqModels.AccountModel);
 
 exports.routing = function (app, validator) {
+    
     /*************************** Common Routes ***************************/
+    
     app.get('/', authController.validateToken, authController.validatePermission, commonController.getWelcomePage)
     app.post('/signIn', authController.validateToken, authController.validatePermission, validator.body(accountModel.signIn), commonController.getWelcomePage)
 }
